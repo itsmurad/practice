892dcde086f37ad51893d71ed8a589e67adc06e6
itsmurad <118719384+itsmurad@users.noreply.github.com>
2023-07-13 11:42:14 +0400

    Update app.js

--- rent-a-car/api/app.js
+++ rent-a-car/api/app.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const Connect = require('./db/Connections');
+const { sequelize, Sequelize } = require('./models');
+const app = express();
+const port = 3000;
 
+const carsRouter = require('./routers/car')
+
+Connect()
+
+app.use(express.json())
+
+app.get('', (req, res) => res.send('Hello World!'));
+
+app.use(carsRouter);
+
+app.listen(port, () => console.log(`Express app running on port ${port}!`));
